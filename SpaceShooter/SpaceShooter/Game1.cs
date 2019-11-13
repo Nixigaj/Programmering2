5b1a0fc0f309a98771bb763f9c020af820f9daa0
Nixigaj <danballnixigaj@gmail.com>
2019-11-13 11:34:38 +0100

    Onsdag programmeringslektion

--- SpaceShooter/SpaceShooter/Game1.cs
+++ SpaceShooter/SpaceShooter/Game1.cs
@@ -107,12 +107,12 @@ namespace SpaceShooter
             {
                 if (keyboardState.IsKeyDown(Keys.D))
                 {
-                    if (keyboardState.IsKeyDown(Keys.Space)) ship_vector.X += ship_speed.X + boostValue;
+                    if (keyboardState.IsKeyDown(Keys.LeftShift)) ship_vector.X += ship_speed.X + boostValue;
                     else ship_vector.X += ship_speed.X;
                 }
                 if (keyboardState.IsKeyDown(Keys.A))
                 {
-                    if (keyboardState.IsKeyDown(Keys.Space)) ship_vector.X -= ship_speed.X + boostValue;
+                    if (keyboardState.IsKeyDown(Keys.LeftShift)) ship_vector.X -= ship_speed.X + boostValue;
                     else ship_vector.X -= ship_speed.X;
                 }
             }
@@ -121,12 +121,12 @@ namespace SpaceShooter
             {
                 if (keyboardState.IsKeyDown(Keys.S))
                 {
-                    if (keyboardState.IsKeyDown(Keys.Space)) ship_vector.Y += ship_speed.Y + boostValue;
+                    if (keyboardState.IsKeyDown(Keys.LeftShift)) ship_vector.Y += ship_speed.Y + boostValue;
                     else ship_vector.Y += ship_speed.Y;
                 }
                 if (keyboardState.IsKeyDown(Keys.W))
                 {
-                    if (keyboardState.IsKeyDown(Keys.Space)) ship_vector.Y -= ship_speed.Y + boostValue;
+                    if (keyboardState.IsKeyDown(Keys.LeftShift)) ship_vector.Y -= ship_speed.Y + boostValue;
                     else ship_vector.Y -= ship_speed.Y;
                 }
             }
@@ -148,7 +148,7 @@ namespace SpaceShooter
         {
 
 
-            if (!Keyboard.GetState().IsKeyDown(Keys.LeftShift)) GraphicsDevice.Clear(backgroundColor);
+            if (!Keyboard.GetState().IsKeyDown(Keys.Space)) GraphicsDevice.Clear(backgroundColor);
 
             spriteBatch.Begin();   // Starta "bildUppritaren"
             spriteBatch.Draw(ship_texture, ship_vector, Color.White);
@@ -157,6 +157,46 @@ namespace SpaceShooter
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// Gets an array of colors for a Top Down Gradient.
+        /// </summary>
+        /// <param name="width">The width of the color array.</param>
+        /// <param name="height">The height of the color array.</param>
+        /// <returns>Color Array.</returns>
+        private static Color[] GetGradientColors(uint width, uint height)
+        {
+            //Declare variables
+            Color[] result;
+            float
+                increment;
+            int color;
+
+            //Determine that both height and width are greater than 0
+            if (!(width > 0 && height > 0))
+            //exit the function with a null color array
+            { return null; }
+
+            //Setup the result array
+            result = new Color[width * height];
+
+            //Calculate the increment values
+            increment = (float)255 / (result.Length);
+
+            //Loop through each color
+            for (int i = 0; i < result.Length; i++)
+            {
+                color = (int)(increment * i);
+
+                result[i] = new Color(
+                    color,
+                    color,
+                    color);
+            }
+
+            //return the color
+            return result;
         }
     }
 }
