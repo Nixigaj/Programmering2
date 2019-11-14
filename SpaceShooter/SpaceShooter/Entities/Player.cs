c857c9d453451effd4ef449f1685f0e2b3323767
Nixigaj <danballnixigaj@gmail.com>
2019-11-14 14:49:33 +0100

    Torsdag programmeringslektion

---
+++ SpaceShooter/SpaceShooter/Entities/Player.cs
@@ -0,0 +1,33 @@
+﻿/// KONTROLLER:
+///
+/// W: Framåt
+/// A: Vänster
+/// S: Bakåt
+/// D: Höger
+/// Mellanslag: Färdas snabbare
+/// Vänster shift: Lämna svans efter dig
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SpaceShooter.Entities
+{
+    class Player
+    {
+        Texture2D ship_texture;
+        Vector2 ship_vector;
+        Vector2 ship_speed;
+
+        public Player(Texture2D texture, float X, float Y, float speedX, float speedY)
+        {
+
+        }
+    }
+}
---
+++ SpaceShooter/SpaceShooter/Tools/Visual/Gradient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace SpaceShooter.Tools.Visual
+{
+    class Gradient
+    {
+        private static Color[] GetGradientColors(uint width, uint height)
+        {
+            //Declare variables
+            Color[] result;
+            float increment;
+            int color;
+
+            //Determine that both height and width are greater than 0
+            if (!(width > 0 && height > 0))
+            //exit the function with a null color array
+            { return null; }
+
+            //Setup the result array
+            result = new Color[width * height];
+
+            //Calculate the increment values
+            increment = (float)255 / (result.Length);
+
+            //Loop through each color
+            for (int i = 0; i < result.Length; i++)
+            {
+                color = (int)(increment * i);
+
+                result[i] = new Color(
+                    color,
+                    color,
+                    color);
+            }
+
+            //return the color
+            return result;
+        }
+    }
+}
