aa82b6c5678b119f352150be0ae130c756e065dd
Nixigaj <danballnixigaj@gmail.com>
2020-01-13 12:53:04 +0100

    Lite mera ändringar

--- Spelprojekt/Background/RecrusiveTexture.cs
+++ Spelprojekt/Background/RecrusiveTexture.cs
@@ -10,24 +10,33 @@ namespace Spelprojekt.Background
     {
         // Hur många paneler som ska ritas ut
         private int panelAmount;
-        // 
+        // När nästa rad av texturer ska ritas
         private int lineInterval;
 
-        // Lista på alla moving objects som ska ritas ut.
+        // Lista på alla moving objects som ska ritas ut
         private List<MovingObject> panels;
 
         public RecrusiveTexture(Texture2D texture, float X, float Y, float speedX, float speedY, int iterationsX, int iterationsY)
         {
             lineInterval = iterationsX;
             panelAmount = iterationsX * iterationsY;
-            
+
+            for ()
         }
 
-        public override void Update(GameWindow window)
+        public void Update(GameWindow window)
         {
             throw new NotImplementedException();
         }
 
-        public
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, vector, Color.White);
+        }
+
+        public float X { get { return vector.X; } }
+        public float Y { get { return vector.Y; } }
+        public float Width { get { return texture.Width; } }
+        public float Height { get { return texture.Height; } }
     }
 }
